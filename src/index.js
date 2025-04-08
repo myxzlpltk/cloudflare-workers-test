9f555dd80a3bebeaa256a0dc910eb16a79e883d4
Saddam Azy <saddam.coder@gmail.com>
2025-04-08 09:19:16 +0700

    Add configuration files for Prettier, EditorConfig, and Vitest; initialize Cloudflare Worker setup

---
+++ src/index.js
@@ -0,0 +1,21 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run "npm run dev" in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run "npm run deploy" to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export default {
+	async fetch(request, env, ctx) {
+	  const response = await fetch("https://api.ipify.org?format=json");
+	  const data = await response.json();
+  
+	  return new Response(data.ip ?? "unknown", {
+		headers: { "content-type": "text/plain" },
+	  });
+	},
+  };
+  
